d11f1c2f63c2dbafb0ffd8bf55f3c4c5afcc7a68
Mike Estrada <mestrada@nu.edu>
2019-11-18 16:17:20 -0800

    Add php test file for code review test

---
+++ plugins/custom-plugin/custom-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: NUSA Custom Plugin
+ * Description: Custom Plugin for code tests.
+ * Version: 1.0.0
+ * Author: Mike Estrada
+ */
+
+// Broken comment for the code sniffer to find
+$variable_name = '';
+
+if ( $variable_name == '' ) {
+	$variable_name = 'test';
+}
